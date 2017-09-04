8cddc3de94ab8068c4b00dd1201cd9f30533ca0d
wangbadada <3041846076@qq.com>
2017-09-04 22:42:44 +0800

    0904

--- day2.py
+++ day2.py
@@ -1,32 +1,41 @@
-# def print_lol(the_list,level):
-#     for each_item in the_list:
-#         if isinstance(each_item,list):
-#             print_lol(each_item,level+1)
-#         else:
-#             for tab_stop in range(level):
-#                 print("\t",end='')
-#             print(each_item)
-#
-# movies=["The Holy Grail",1975,"Terry Jones&Terry Gilliam",91,
-# 	["Graham Chapman",
-# 	 ["Michael Palin","John Cleese","Terry Gilliam","Eric Idle","Terry Jones"]]]
-#
-# print_lol(movies,0)
-'''data=open('sketch1.txt')
+# 创建函数： def[函数名] （[参数]）：[函数代码块]
+'''
+def print_lol(the_list,level):
+     for each_item in the_list:
+         if isinstance(each_item,list):
+             print_lol(each_item,level+1)
+         else:
+             for tab_stop in range(level):
+                 print("\t",end='')
+             print(each_item)
+movies=["The Holy Grail",1975,"Terry Jones&Terry Gilliam",91,
+	 ["Graham Chapman",
+ 	 ["Michael Palin","John Cleese","Terry Gilliam","Eric Idle","Terry Jones"]]]
+
+print_lol(movies,0)
+'''
+
+#open()打开数据作为单个文本行
+'''
+data=open('sketch1.txt')
 for each_line in data:
     (role,line_spoken)=each_line.split(':',1)
     print(role,end='')
     print(' said: ',end='')
     print(line_spoken,end='')
-data.close()'''
-'''try:
+data.close()
+'''
+#异常处理
+'''
+try:
     data=open('missing.txt')
     print(data.readline(),end='')
 except IOError:
     print('File error')
 finally:
     if 'data' in locals():
-        data.colse()'''
+        data.colse()
+        '''
 
 '''try:
     data=open('missing.txt')
@@ -50,7 +59,7 @@ finally:
         print('it is...',file=data)
 except IOError as err:
     print('File Error:'+str(err))'''
-man=[]
+'''man=[]
 other=[]
 try:
     data=open("sketch1.txt")
@@ -66,7 +75,7 @@ try:
             pass
     data.close()
 except IOError:
-    print('The data file is missing!')
+    print('The data file is missing!')'''
 '''try:
     man_file = open('man_data.txt','w')
     other_file = open('other_data.txt','w')
@@ -80,7 +89,7 @@ finally:
         man_file.close()
     if 'other_file' in locals():
         other_file.close()'''
-try:
+'''try:
     with open('man_data.txt','w') as man_file:
         print('man',file=man_file)
     with open('other_data.txt','w') as other_file:
@@ -89,7 +98,7 @@ except IOError as err:
     print('File Error: '+str(err))
 finally:
     man_file.close()
-    other_file.close()
+    other_file.close()'''
 
 
 '''
