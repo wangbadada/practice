c915ff0e27622126fa8a7df82e6db82e4609dfc4
wangbadada <3041846076@qq.com>
2017-09-05 22:41:07 +0800

    gbk-utf8

--- day2.py
+++ day2.py
@@ -129,3 +129,58 @@ try:
     other_file.close()
 except IOError:
     print('File Error.')'''
+
+
+#indent缩进
+#isinstance判断each_item是否为列表
+'''def print_lol(the_list,indent=False,level=0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print('\t',end='')
+                print(each_item)'''
+
+#standard output（标准输出）这是使用“print()”BIF时代码写数据的默认位置。这通常是屏幕。
+# 在python中，标准输出是指“sys.stdout”，可以从标准库的‘sys’模块导入。
+# 向print_lol()函数增加第4个参数，用来标识把数据写入哪个位置。
+# 一定要为这个参数提供一个缺省值sys.stdout，这样如果调用这个函数时没有指定文件对象则会依然写至屏幕。
+import sys
+def print_lol(the_list,indent=False,level=0,fh=sys.stdout):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1,fh)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print('\t',end='',file=fh)
+            print(each_item,file=fh)
+man=[]
+other=[]
+try:
+    data=open("sketch1.txt")
+    for each_line in data:
+        try:
+            (role,line_spoken)=each_line.split(":",1)
+            line_spoken=line_spoken.strip()
+            if role=='Man':
+                man.append(line_spoken)
+            elif role=='Other Man':
+                other.append(line_spoken)
+        except ValueError:
+            pass
+    data.close()
+except IOError:
+    print('The data file is missing!')
+try:
+    with open('man_data.txt','w') as man_file:
+        print_lol(man,fh=man_file)
+    with open('other_data.txt','w') as other_file:
+        print_lol(other,fh=other_file)
+except IOError as err:
+    print('File Error: '+str(err))
+finally:
+    man_file.close()
+    other_file.close()
